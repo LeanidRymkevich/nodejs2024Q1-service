33b3581b1ca35e201465a4e58cc1f5e14dac0f2a
LeanidRymkevich <leanidrymkevich@gmail.com>
2024-03-10 11:50:01 +0300

    feat: add album entity and dto

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,24 @@
-export class CreateAlbumDto {}
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+  ValidateIf,
+} from 'class-validator';
+
+const MIN_YEAR = 0;
+
+export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @Min(MIN_YEAR)
+  year: number;
+
+  @ValidateIf((value: unknown): boolean => value === null)
+  @IsString()
+  @IsNotEmpty()
+  artistId: string | null;
+}
